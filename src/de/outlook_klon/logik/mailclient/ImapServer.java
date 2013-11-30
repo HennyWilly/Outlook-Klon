69e2acd742f1e9950897a881af7287c8868db5a2
HennyWilly <henny-willy@hotmail.de>
2013-11-30 12:52:13 +0100

    Methode zur Rückgabe des MailStores für IMAP/IMAPS implementiert. Bisher nur mit IMAPS getestet.

--- src/de/outlook_klon/logik/mailclient/ImapServer.java
+++ src/de/outlook_klon/logik/mailclient/ImapServer.java
@@ -1,5 +1,13 @@
 package de.outlook_klon.logik.mailclient;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Store;
+
 /**
  * Diese Klasse stellt einen IMAP-Server dar.
  * 
@@ -13,5 +21,35 @@ public class ImapServer extends EmpfangsServer {
 	 */
 	public ImapServer(ServerSettings settings) {
 		super(settings);
+	}
+
+	@Override
+	public Store getMailStore(final String user, final String pw) throws NoSuchProviderException {
+		Authenticator auth = new Authenticator() {
+			@Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pw);
+            }
+		};
+		
+		Properties props = System.getProperties();
+		props.put("mail.imap.host", settings.getHost());
+		props.put("mail.imap.port", settings.getPort());
+		props.put("mail.imap.auth", true);
+
+		if(settings.getVerbingungssicherheit() == Verbindungssicherheit.SSL_TLS) {
+			props.put("mail.imap.ssl.enable", true);
+		}
+		
+		Session session = Session.getInstance(props, auth);
+		session.setDebug(true);
+		
+		Store store = null;
+		if(settings.getVerbingungssicherheit() == Verbindungssicherheit.SSL_TLS)
+			store = session.getStore("imaps");
+		else
+			store = session.getStore("imap");
+		
+		return store;
 	}
 }
