fd4b9d7b622729905604c42712fa43bc1d31ccd8
HennyWilly <henny-willy@hotmail.de>
2013-11-30 12:50:11 +0100

    Abstrakte Methode zur Rückgabe des MailStores hinzugefügt.

--- src/de/outlook_klon/logik/mailclient/EmpfangsServer.java
+++ src/de/outlook_klon/logik/mailclient/EmpfangsServer.java
@@ -1,5 +1,8 @@
 package de.outlook_klon.logik.mailclient;
 
+import javax.mail.NoSuchProviderException;
+import javax.mail.Store;
+
 /**
  * Abstrakte Basisklasse für alle Mailserver, über die Mails empfangen werden können.
  * Stellt grundlegende Funtionen zum Empfangen von Mails bereit.
@@ -10,4 +13,12 @@ public abstract class EmpfangsServer extends MailServer {
 	protected EmpfangsServer(ServerSettings settings) {
 		super(settings);
 	}
+
+	/**
+	 * Gibt den Store zurück, der die E-Mails des Anwenders enthällt
+	 * @param user Benutzername des Empfängers
+	 * @param pw Passwort des Empfängers
+	 * @return Store-Objekt, über welches man auf die Mails zugreifen kann
+	 */
+	public abstract Store getMailStore(String user, String pw) throws NoSuchProviderException;
 }
