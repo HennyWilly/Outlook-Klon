30aae5a6b4e2bdd33717123aeca575838d2f4ccf
HennyWilly <henny-willy@hotmail.de>
2013-12-01 14:50:15 +0100

    Roh-Frame zur Verwaltung der MailAccounts erstellt.

---
+++ src/de/outlook_klon/gui/KontoverwaltungFrame.java
@@ -0,0 +1,22 @@
+package de.outlook_klon.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import de.outlook_klon.logik.Benutzer;
+
+public class KontoverwaltungFrame extends JFrame implements ActionListener {
+	private static final long serialVersionUID = -5036893845172118794L;
+
+	public KontoverwaltungFrame(Benutzer benutzer) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
