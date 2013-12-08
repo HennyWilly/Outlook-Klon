877cbb6dd5371bddf042c0e82c2260be9fe3a4ca
HennyWilly <henny-willy@hotmail.de>
2013-12-08 17:18:06 +0100

    Methode zum Prüfen des Logins implementiert

--- src/de/outlook_klon/logik/mailclient/EmpfangsServer.java
+++ src/de/outlook_klon/logik/mailclient/EmpfangsServer.java
@@ -1,5 +1,6 @@
 package de.outlook_klon.logik.mailclient;
 
+import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Store;
 
@@ -10,6 +11,8 @@ import javax.mail.Store;
  * @author Hendrik Karwanni
  */
 public abstract class EmpfangsServer extends MailServer {
+	private static final long serialVersionUID = -6475925504329915182L;
+
 	protected EmpfangsServer(ServerSettings settings, String serverTyp) {
 		super(settings, serverTyp);
 	}
@@ -21,4 +24,26 @@ public abstract class EmpfangsServer extends MailServer {
 	 * @return Store-Objekt, über welches man auf die Mails zugreifen kann
 	 */
 	public abstract Store getMailStore(String user, String pw) throws NoSuchProviderException;
+	
+	public boolean prüfeLogin(String benutzername, String passwort){
+		Store store = null;
+		
+		try {
+			store = getMailStore(benutzername, passwort);
+			store.connect(settings.getHost(), settings.getPort(), benutzername, passwort);
+		} catch (Exception ex) {
+			return false;
+		} finally {
+			if(store != null && store.isConnected()) {
+				try {
+					store.close();
+				} catch (MessagingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return true;
+	}
 }
