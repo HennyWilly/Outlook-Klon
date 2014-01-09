0098f3b8d5d532d45291250f9885bbd4345fb8a3
HennyWilly <henny-willy@hotmail.de>
2014-01-09 16:32:32 +0100

    Eclipse Auto-Format

--- src/de/outlook_klon/logik/kalendar/Termin.java
+++ src/de/outlook_klon/logik/kalendar/Termin.java
@@ -10,13 +10,14 @@ import de.outlook_klon.logik.Benutzer;
 import de.outlook_klon.logik.kontakte.Kontakt;
 
 /**
- * Dies ist eine Datenklasse, die die Daten von einem Termin des Benutzers speichert.
+ * Dies ist eine Datenklasse, die die Daten von einem Termin des Benutzers
+ * speichert.
  * 
  * @author Hendrik Karwanni
  */
-public class Termin implements Serializable, Comparable<Termin>{
+public class Termin implements Serializable, Comparable<Termin> {
 	private static final long serialVersionUID = 6997576125673406382L;
-	
+
 	private String mBetreff;
 	private String mOrt;
 	private Date mStart;
@@ -26,7 +27,7 @@ public class Termin implements Serializable, Comparable<Termin>{
 	private String mKontakt;
 	private Status mStatus;
 	private InternetAddress[] mAdressen;
-	
+
 	/**
 	 * Diese Aufzählung stellt die möglichen Zustände eines Termins dar
 	 */
@@ -35,22 +36,29 @@ public class Termin implements Serializable, Comparable<Termin>{
 		 * Dem Termin wurde zugesagt
 		 */
 		zugesagt,
-		
+
 		/**
 		 * Der Termin wurde abgelehnt
 		 */
-		anbelehnt   //wohl eher abgelehnt :P
+		abgelehnt
 	}
-	
+
 	/**
 	 * Erstellt eine neue Instanz der Klasse mit den übergebenen Werten
-	 * @param betreff Betreff des Termins
-	 * @param ort Ort des Termins
-	 * @param start Startzeitpunkt des Termins
-	 * @param ende Endzeitpunkt des Termins
-	 * @param text Text des Termins
+	 * 
+	 * @param betreff
+	 *            Betreff des Termins
+	 * @param ort
+	 *            Ort des Termins
+	 * @param start
+	 *            Startzeitpunkt des Termins
+	 * @param ende
+	 *            Endzeitpunkt des Termins
+	 * @param text
+	 *            Text des Termins
 	 */
-	public Termin(String betreff, String ort, Date start, Date ende, String text, String benutzer, String kontakt) {	
+	public Termin(String betreff, String ort, Date start, Date ende,
+			String text, String benutzer, String kontakt) {
 		setBetreff(betreff);
 		setOrt(ort);
 		setStartUndEnde(start, ende);
@@ -58,17 +66,14 @@ public class Termin implements Serializable, Comparable<Termin>{
 		setBenutzerkonto(benutzer);
 		setKontakt(kontakt);
 		setStatus(Status.zugesagt);
-		
-		ArrayList <InternetAddress> temp = new ArrayList <InternetAddress>(2);
-		for(Kontakt k: Benutzer.getInstanz().getKontakte()){
-			if(k.getAnzeigename()==kontakt)
-			{
-				if(k.getMail1()!=null)
-				{
+
+		ArrayList<InternetAddress> temp = new ArrayList<InternetAddress>(2);
+		for (Kontakt k : Benutzer.getInstanz().getKontakte()) {
+			if (k.getAnzeigename() == kontakt) {
+				if (k.getMail1() != null) {
 					temp.add(k.getMail1());
 				}
-				if(k.getMail2()!=null)
-				{
+				if (k.getMail2() != null) {
 					temp.add(k.getMail2());
 				}
 				break;
@@ -79,7 +84,9 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Setter für den Betreff des Termins
-	 * @param betreff Zu setzender Betreff
+	 * 
+	 * @param betreff
+	 *            Zu setzender Betreff
 	 */
 	public void setBetreff(String betreff) {
 		this.mBetreff = betreff;
@@ -87,6 +94,7 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Getter für den Betreff des Termins
+	 * 
 	 * @return Betreff
 	 */
 	public String getBetreff() {
@@ -95,7 +103,9 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Setter für den Ort des Termins
-	 * @param ort Zu setzender Ort
+	 * 
+	 * @param ort
+	 *            Zu setzender Ort
 	 */
 	public void setOrt(String ort) {
 		this.mOrt = ort;
@@ -103,26 +113,32 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Getter für den Ort des Termins
+	 * 
 	 * @return Ort
 	 */
 	public String getOrt() {
 		return mOrt;
 	}
-	
+
 	/**
 	 * Setter für den Start- und Endzeitpunkt des Termins
-	 * @param start Zu setzender Startzeitpunkt
-	 * @param ende Zu setzender Endzeitpunkt
+	 * 
+	 * @param start
+	 *            Zu setzender Startzeitpunkt
+	 * @param ende
+	 *            Zu setzender Endzeitpunkt
 	 */
 	public void setStartUndEnde(Date start, Date ende) {
-		if(start.after(ende))
-			throw new RuntimeException("Der Startzeitpunkt darf nicht hinter dem Endzeitpunkt liegen");
+		if (start.after(ende))
+			throw new RuntimeException(
+					"Der Startzeitpunkt darf nicht hinter dem Endzeitpunkt liegen");
 		mStart = new Date(start.getTime());
 		mEnde = new Date(ende.getTime());
 	}
-	
+
 	/**
 	 * Getter für den Startzeitpunkt des Termins
+	 * 
 	 * @return Startzeitpunkt
 	 */
 	public Date getStart() {
@@ -131,6 +147,7 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Getter für den Endzeitpunkt des Termins
+	 * 
 	 * @return Endzeitpunkt
 	 */
 	public Date getEnde() {
@@ -139,7 +156,9 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Setter für die Beschreibung des Termins
-	 * @param text Zu setzende Beschreibung
+	 * 
+	 * @param text
+	 *            Zu setzende Beschreibung
 	 */
 	public void setText(String text) {
 		this.mText = text;
@@ -147,41 +166,48 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Getter für die Beschreibung des Termins
+	 * 
 	 * @return Beschreibung
 	 */
 	public String getText() {
 		return mText;
 	}
-	
+
 	/**
 	 * Setter für das zugeordnete Benutzerkonto des Termins
-	 * @param benutzer Zu setzende Benutzerkonto
+	 * 
+	 * @param benutzer
+	 *            Zu setzende Benutzerkonto
 	 */
 	public void setBenutzerkonto(String benutzer) {
 		this.mBenutzerkonto = benutzer;
 	}
-	
+
 	/**
 	 * Getter für das zugeordnete Benutzerkonto des Termins
+	 * 
 	 * @return Benutzerkonto
 	 */
 	public String getBenutzerkonto() {
 		return mBenutzerkonto;
 	}
-	
+
 	/**
 	 * Getter für den zugeordneten Kontakt des Termins
+	 * 
 	 * @return Kontakt
 	 */
-	public String getKontakt(){
+	public String getKontakt() {
 		return mKontakt;
 	}
-	
+
 	/**
 	 * Setter für den zugeordneten Kontakt des Termins
-	 * @param kontakt Zu setzende Kontakt
+	 * 
+	 * @param kontakt
+	 *            Zu setzende Kontakt
 	 */
-	public void setKontakt(String kontakt){
+	public void setKontakt(String kontakt) {
 		this.mKontakt = kontakt;
 	}
 
@@ -192,6 +218,7 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Gibt den Status des Termins zurück
+	 * 
 	 * @return Status des Termins
 	 */
 	public Status getStatus() {
@@ -200,7 +227,9 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Setzt den Status des Termins
-	 * @param mStatus Status des Termins
+	 * 
+	 * @param mStatus
+	 *            Status des Termins
 	 */
 	public void setStatus(Status mStatus) {
 		this.mStatus = mStatus;
@@ -208,15 +237,18 @@ public class Termin implements Serializable, Comparable<Termin>{
 
 	/**
 	 * Gibt die verknüpfen Adressen zum Termin zurück
+	 * 
 	 * @return Adressen zum Termin
 	 */
 	public InternetAddress[] getAdressen() {
-		return mAdressen;  
+		return mAdressen;
 	}
 
 	/**
 	 * Setzt die verknüpfen Adressen zum Termin
-	 * @param mAdressen Adressen zum Termin
+	 * 
+	 * @param mAdressen
+	 *            Adressen zum Termin
 	 */
 	public void setAdressen(InternetAddress[] mAdressen) {
 		this.mAdressen = mAdressen;
--- src/de/outlook_klon/logik/kalendar/Terminkalender.java
+++ src/de/outlook_klon/logik/kalendar/Terminkalender.java
@@ -2,6 +2,7 @@ package de.outlook_klon.logik.kalendar;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
@@ -13,7 +14,7 @@ import java.util.Iterator;
  */
 public class Terminkalender implements Iterable<Termin>, Serializable {
 	private static final long serialVersionUID = 3595324672069971302L;
-	
+
 	private ArrayList<Termin> mTermine;
 
 	/**
@@ -30,135 +31,156 @@ public class Terminkalender implements Iterable<Termin>, Serializable {
 
 	/**
 	 * Fügt den übergebenen Termin der Verwaltung hinzu
-	 * @param termin Der hinzuzufügende Termin
+	 * 
+	 * @param termin
+	 *            Der hinzuzufügende Termin
 	 */
 	public void addTermin(Termin termin) {
 		mTermine.add(termin);
 	}
-	
+
 	/**
 	 * Löscht den übergebenen Termin aus der Verwaltung
-	 * @param termin Zu löschender Termin
+	 * 
+	 * @param termin
+	 *            Zu löschender Termin
 	 */
 	public void löscheTermin(Termin termin) {
 		mTermine.remove(termin);
 	}
-	
+
 	/**
-	 * Gibt zurück, ob sich die Termine der Verwaltung mit dem übergebenen Termin überschneiden
-	 * @param a Zu vergleichender Termin
+	 * Gibt zurück, ob sich die Termine der Verwaltung mit dem übergebenen
+	 * Termin überschneiden
+	 * 
+	 * @param a
+	 *            Zu vergleichender Termin
 	 * @return true, wenn sich mindestens ein Termin überschneidet; sonst false
 	 */
-	public boolean ueberschneidung(Termin a){
+	public boolean ueberschneidung(Termin a) {
 		Date startA = a.getStart();
 		Date endeA = a.getEnde();
-		
-		for(Termin b : mTermine)
-		{
+
+		for (Termin b : mTermine) {
 			Date startB = b.getStart();
 			Date endeB = b.getEnde();
-					//IF-Abfrage des Todes
-			if ((startA.before(startB) && endeA.after(startB)) || (startA.before(endeB) && endeA.after(endeB)) 
-					|| (startB.before(startA) && endeB.after(startA)) || (startB.before(endeA) && endeB.after(endeA)))
-			
+			// IF-Abfrage des Todes
+			if ((startA.before(startB) && endeA.after(startB))
+					|| (startA.before(endeB) && endeA.after(endeB))
+					|| (startB.before(startA) && endeB.after(startA))
+					|| (startB.before(endeA) && endeB.after(endeA)))
+
 			{
 				return true;
 			}
 		}
 		return false;
-	}	
-	
+	}
+
 	/**
 	 * Gibt den Termin zurück, der am spätesten beginnt
+	 * 
 	 * @return Termin-Objekt, das zeitlich am spätesten beginnt
 	 */
 	public Termin getOldest() {
-		if(mTermine.size() == 0)
+		if (mTermine.size() == 0)
 			return null;
-		
+
 		Termin t = mTermine.get(0);
-		
-		for(Termin a : mTermine) {
+
+		for (Termin a : mTermine) {
 			if (a.getStart().before(t.getStart()))
-				t=a;
+				t = a;
 		}
 		return t;
 	}
-	
+
 	/**
 	 * Gibt die Anzahl der Termine der Verwaltung zurück
+	 * 
 	 * @return Anzahl der Termine
 	 */
 	public int getSize() {
 		return mTermine.size();
 	}
-	
+
 	/**
 	 * Gibt alle Termine in der übergebenen Zeitspanne zurück
-	 * @param start Startzeit der Auswertung
-	 * @param ende Endzeit der Auswertung
+	 * 
+	 * @param start
+	 *            Startzeit der Auswertung
+	 * @param ende
+	 *            Endzeit der Auswertung
 	 * @return Termine innerhalb des intervalls
 	 */
 	public Termin[] getTermine(Date start, Date ende) {
-		if(ende.before(start))
-			throw new IllegalArgumentException("Der Startzeitpunkt darf nicht hinter dem Endzeitpunkt liegen");
-		
+		if (ende.before(start))
+			throw new IllegalArgumentException(
+					"Der Startzeitpunkt darf nicht hinter dem Endzeitpunkt liegen");
+
 		ArrayList<Termin> liste = new ArrayList<Termin>();
-		for(Termin termin : mTermine) {
+		for (Termin termin : mTermine) {
 			Date startZeit = termin.getStart();
-			if(start.equals(startZeit) || (startZeit.after(start) && startZeit.before(ende)))  {
+			if (start.equals(startZeit)
+					|| (startZeit.after(start) && startZeit.before(ende))) {
 				liste.add(termin);
 			}
 		}
 		return liste.toArray(new Termin[liste.size()]);
 	}
-	
+
 	/**
 	 * Gibt alle Termine des aktuellen Tages zurück
+	 * 
 	 * @return Termine des aktuellen Tages
 	 */
 	public Termin[] getTermine() {
 		Date jetzt = new Date();
-		
+
 		Date start = null;
 		Date ende = null;
 		GregorianCalendar c = new GregorianCalendar();
-		
+
 		c.setTime(jetzt);
-		c.set(GregorianCalendar.HOUR, 0);		//Setzt den Eintrag der Stunden auf 0
-		c.set(GregorianCalendar.MINUTE, 0);		//Setzt den Eintrag der Minuten auf 0
-		c.set(GregorianCalendar.SECOND, 0);		//Setzt den Eintrag der Sekunden auf 0
-		
-		start = c.getTime();					//Übergebener Tag mit der Uhrzeit 00:00:00
-		c.add(GregorianCalendar.DAY_OF_YEAR, 1);
-		ende = c.getTime();					//Tag um 1 höher als time1
-		
+		c.set(Calendar.HOUR, 0); // Setzt den Eintrag der Stunden auf 0
+		c.set(Calendar.MINUTE, 0); // Setzt den Eintrag der Minuten auf 0
+		c.set(Calendar.SECOND, 0); // Setzt den Eintrag der Sekunden auf 0
+
+		start = c.getTime(); // Übergebener Tag mit der Uhrzeit 00:00:00
+		c.add(Calendar.DAY_OF_YEAR, 1);
+		ende = c.getTime(); // Tag um 1 höher als time1
+
 		return getTermine(start, ende);
 	}
-	
+
 	/**
-	 * Entfernt alle Termine aus der Verwaltung, die am übergebenen Tag stattfinden
-	 * @param tag Date-Objekt, welches den Tag enthällt, an dem alle Termine entfernt werden
+	 * Entfernt alle Termine aus der Verwaltung, die am übergebenen Tag
+	 * stattfinden
+	 * 
+	 * @param tag
+	 *            Date-Objekt, welches den Tag enthällt, an dem alle Termine
+	 *            entfernt werden
 	 */
 	public void absagen(Date tag) {
 		Date time1 = null;
 		Date time2 = null;
 		GregorianCalendar c = new GregorianCalendar();
-		
+
 		c.setTime(tag);
-		c.set(GregorianCalendar.HOUR, 0);		//Setzt den Eintrag der Stunden auf 0
-		c.set(GregorianCalendar.MINUTE, 0);		//Setzt den Eintrag der Minuten auf 0
-		c.set(GregorianCalendar.SECOND, 0);		//Setzt den Eintrag der Sekunden auf 0
-		
-		time1 = c.getTime();					//Übergebener Tag mit der Uhrzeit 00:00:00
-		c.add(GregorianCalendar.DAY_OF_YEAR, 1);
-		time2 = c.getTime();					//Tag um 1 höher als time1
-		
-		for(int i = 0; i < mTermine.size(); i++) {
+		c.set(Calendar.HOUR, 0); // Setzt den Eintrag der Stunden auf 0
+		c.set(Calendar.MINUTE, 0); // Setzt den Eintrag der Minuten auf 0
+		c.set(Calendar.SECOND, 0); // Setzt den Eintrag der Sekunden auf 0
+
+		time1 = c.getTime(); // Übergebener Tag mit der Uhrzeit 00:00:00
+		c.add(Calendar.DAY_OF_YEAR, 1);
+		time2 = c.getTime(); // Tag um 1 höher als time1
+
+		for (int i = 0; i < mTermine.size(); i++) {
 			Termin termin = mTermine.get(i);
 			Date start = termin.getStart();
-			
-			if(start.equals(time1) || (start.after(time1) && start.before(time2))) {
+
+			if (start.equals(time1)
+					|| (start.after(time1) && start.before(time2))) {
 				löscheTermin(termin);
 				i--;
 			}
