9d2382df9dd94a5acfd68cded13ef2e5839850cf
HennyWilly <henny-willy@hotmail.de>
2016-04-09 09:12:49 +0200

    Intermediate commit

---
+++ src/main/java/de/outlookklon/serializers/mixins/AddressMixIn.java
@@ -0,0 +1,22 @@
+package de.outlookklon.serializers.mixins;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * MixIn for class {@link javax.mail.Address}. Adds a class id to the serialized
+ * data in order to store type information.
+ *
+ * @author Hendrik Karwanni
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+public abstract class AddressMixIn {
+
+    /**
+     * Ignore method {@link javax.mail.Address#getType()} during serialization.
+     *
+     * @return Some interesting stuff....
+     */
+    @JsonIgnore
+    public abstract String getType();
+}
---
+++ src/main/java/de/outlookklon/serializers/mixins/InternetAddressMixIn.java
@@ -0,0 +1,26 @@
+package de.outlookklon.serializers.mixins;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import de.outlookklon.serializers.InternetAddressDeserializer;
+
+/**
+ * MixIn for class {@link javax.mail.internet.InternetAddress}. In order to
+ * deserialize non-public sub classes of
+ * {@link javax.mail.internet.InternetAddress}, a custom
+ * {@link com.fasterxml.jackson.databind.JsonDeserializer} is used.
+ *
+ * @author Hendrik Karwanni
+ */
+@JsonDeserialize(using = InternetAddressDeserializer.class)
+public abstract class InternetAddressMixIn {
+
+    /**
+     * Ignore method {@link javax.mail.internet.InternetAddress#isGroup()}
+     * during serialization.
+     *
+     * @return Some interesting stuff....
+     */
+    @JsonIgnore
+    public abstract boolean isGroup();
+}
